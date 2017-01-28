884f2d5f81719a37cbed97fe8b3ebe204c9d8eea
Yoav Kleinberger <haarcuba@gmail.com>
2017-01-28 23:45:02 +0200

    debugging

--- closer.py
+++ closer.py
@@ -6,8 +6,10 @@ import psutil
 import os
 import cPickle
 import sys
+import logging
+logging.basicConfig( level = logging.INFO )
 
-def killAllDecendants( * args ):
+def killAllAndQuit( * args ):
     me = psutil.Process( os.getpid() )
     for process in me.children( recursive = True ):
         process.terminate()
@@ -21,17 +23,20 @@ def interpret( hexedPickle ):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument( 'popenArgsKwargsInHexedPickle' )
+    parser.add_argument( '--quit-on-input', dest='quitOnInput', action='store_true' )
     arguments = parser.parse_args()
 
-    script = interpret( arguments.popenArgsKwargsInHexedPickle )
-
-    sys.stdout.write( '{}\n'.format( os.getpid() ) )
-    sys.stdout.flush()
-    subprocess.Popen( * script[ 'args' ], ** script[ 'kwargs' ] )
-    signal.signal( signal.SIGTERM, killAllDecendants )
-    signal.signal( signal.SIGINT, killAllDecendants )
-    while True:
-        time.sleep( 1000 )
+    popenDetails = interpret( arguments.popenArgsKwargsInHexedPickle )
+    logging.info( 'will run Popen with: args={args} kwargs={kwargs}'.format( ** popenDetails ) )
+    subprocess.Popen( * popenDetails[ 'args' ], ** popenDetails[ 'kwargs' ] )
+    signal.signal( signal.SIGTERM, killAllAndQuit )
+    signal.signal( signal.SIGINT, killAllAndQuit )
+    if arguments.quitOnInput:
+        raw_input()
+        killAllAndQuit()
+    else:
+        while True:
+            time.sleep( 1000 )
 
 if __name__ == '__main__':
     main()
