207acaaad61669f18b46f96f266d2cd001dacc15
Yoav Kleinberger <haarcuba@gmail.com>
2017-02-13 02:45:47 +0200

    version 0.1.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires = [ 'psutil' ]
 tests_require = []
 
 setup(name='closer',
-      version='0.1.5',
+      version='0.1.6',
       description=README,
       long_description=README,
       url='https://github.com/haarcuba/closer',
