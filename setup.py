dc51c1ebb318beea986e478d41393b6d6dae4601
Yoav Kleinberger <haarcuba@gmail.com>
2018-03-19 20:41:36 +0200

    version 4.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ tests_require = [
         ]
 
 setup(name='closer',
-      version='4.0.0',
+      version='4.0.1',
       description=README,
       long_description=README,
       url='https://github.com/haarcuba/closer',
